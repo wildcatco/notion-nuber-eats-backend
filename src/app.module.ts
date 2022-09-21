3ea3a3229d56c121a9829dbb689abb951d08232c
Jihoo Kim <jihoo94@gmail.com>
2022-09-21 15:43:40 +0900

    TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,28 @@ import { ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloDriver } from '@nestjs/apollo/dist/drivers';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       autoSchemaFile: true,
     }),
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'jihoo',
+      password: '1111',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
   ],
+
   controllers: [],
   providers: [],
 })
