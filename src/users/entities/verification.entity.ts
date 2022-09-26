ff18551fd0d293a2b905cec56d5ea706522bfed9
Jihoo Kim <jihoo94@gmail.com>
2022-09-26 15:51:35 +0900

    Creating Verifications

--- src/users/entities/verification.entity.ts
+++ src/users/entities/verification.entity.ts
@@ -1,6 +1,7 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { BeforeInsert, Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { v4 as uuid4 } from 'uuid';
 import { User } from './user.entity';
 
 @InputType({ isAbstract: true })
@@ -14,4 +15,9 @@ export class Verification extends CoreEntity {
   @OneToOne((type) => User)
   @JoinColumn()
   user: User;
+
+  @BeforeInsert()
+  createCode(): void {
+    this.code = uuid4();
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,11 +9,14 @@ import {
 import { EditProfileInput } from './dtos/edit-profile.dto';
 import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    @InjectRepository(Verification)
+    private readonly verificationsRepository: Repository<Verification>,
     private readonly jwtService: JwtService,
   ) {}
 
@@ -27,8 +30,13 @@ export class UsersService {
       if (exists) {
         return { ok: false, error: 'There is a user with that email already' };
       }
-      await this.usersRepository.save(
+      const user = await this.usersRepository.save(
         this.usersRepository.create({ email, password, role }),
+      );
+      await this.verificationsRepository.save(
+        this.verificationsRepository.create({
+          user,
+        }),
       );
       return { ok: true };
     } catch (error) {
@@ -61,10 +69,21 @@ export class UsersService {
   }
 
   async editProfile(userId: number, { email, password }: EditProfileInput) {
-    // return this.usersRepository.update({ id: userId }, { ...editProfileInput });
     const user = await this.findById(userId);
     if (email) {
       user.email = email;
+      user.verified = false;
+      const verification = await this.verificationsRepository.findOne({
+        where: { user: { id: userId } },
+      });
+      if (verification) {
+        await this.verificationsRepository.delete(verification.id);
+      }
+      await this.verificationsRepository.save(
+        this.verificationsRepository.create({
+          user,
+        }),
+      );
     }
     if (password) {
       user.password = password;
