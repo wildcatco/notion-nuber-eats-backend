3af0d82c9efcad9d739057aeb91607cdb89b1c87
Jihoo <53351097+wildcatco@users.noreply.github.com>
2022-10-04 09:19:07 +0900

    order entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,8 @@ import { AuthModule } from './auth/auth.module';
 import { JwtMiddleware } from './jwt/jwt.middleware';
 import { JwtModule } from './jwt/jwt.module';
 import { MailModule } from './mail/mail.module';
+import { Order } from './orders/entities/order.entity';
+import { OrdersModule } from './orders/orders.module';
 import { Category } from './restaurants/entities/category.entity';
 import { Dish } from './restaurants/entities/dish.entity';
 import { Restaurant } from './restaurants/entities/restaurant.entity';
@@ -56,7 +58,7 @@ import { UsersModule } from './users/users.module';
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: process.env.NODE_ENV === 'dev',
-      entities: [User, Verification, Restaurant, Category, Dish],
+      entities: [User, Verification, Restaurant, Category, Dish, Order],
     }),
     JwtModule.forRoot({
       isGlobal: true,
@@ -71,6 +73,7 @@ import { UsersModule } from './users/users.module';
     AuthModule,
     UsersModule,
     RestaurantsModule,
+    OrdersModule,
   ],
 
   controllers: [],
