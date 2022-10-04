0c6ad16b7797b711b8035637885afe99de3f8fc7
Jihoo <53351097+wildcatco@users.noreply.github.com>
2022-10-05 01:33:52 +0900

    refactor AuthUser decorator

--- src/auth/auth-user.decorator.ts
+++ src/auth/auth-user.decorator.ts
@@ -4,7 +4,6 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 export const AuthUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const gqlContext = GqlExecutionContext.create(context).getContext();
-    const user = gqlContext['user'];
-    return user;
+    return gqlContext['user'];
   },
 );
